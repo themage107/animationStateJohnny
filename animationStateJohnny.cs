ecef6a4202cab1ed7d3d4cfb3b3a9cedef6267d4
Adrian J. Fox <themage107@gmail.com>
2018-11-01 16:48:40 -0700

    During idle, at the end of idle animation, run a random to mix up the idle animation
    This allows for some random idle animations.

---
+++ animationStateJohnny.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationStateJohnny : MonoBehaviour {
+
+    int animState;
+    public Animator jAnim;
+
+    int rndInt;
+
+	// Use this for initialization
+	void Start () {
+        animState = jAnim.GetInteger("animationStateJohnny");
+	}
+	
+	public void randomAnimation()
+    {
+        runCheck();
+    }
+
+    public void resetAnimation()
+    {
+        jAnim.SetInteger("animationStateJohnny", 0);
+    }
+
+    public void runCheck()
+    {
+        rndInt = Random.Range(0, 50);
+        if(rndInt == 5)
+        {
+            jAnim.SetInteger("animationStateJohnny", 1);
+        }
+        if (rndInt == 10)
+        {
+            jAnim.SetInteger("animationStateJohnny", 2);
+        }
+        if (rndInt == 15)
+        {
+            jAnim.SetInteger("animationStateJohnny", 3);
+        }        
+    }
+}
